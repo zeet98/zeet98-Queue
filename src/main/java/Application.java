45548a443a0cfa24dd2f37b0de41dc540e432b76
tedbeast <tedbalashov@gmail.com>
2023-02-10 14:52:49 -0500

    doc fix

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,11 +2,11 @@ import java.util.Deque;
 
 public class Application {
     /**
-     * This class contains a main method that allows you to manually test the Exception lab.
+     * This class contains a main method that allows you to manually test the Queue lab.
      * No changes you make here affect the test case results. You must still run & pass the test cases.
      * All implementation code should be written in the Lab class.
      *
-     * The Lab.mustThrow method should throw an exception.
+     * The Lab methods must perform Queue operations.
      */
     public static void main(String[] args) {
         Lab queueLab = new Lab();
